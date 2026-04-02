4a6361e33e234e0eb873408aecdbcc6b3a5f41b7
Georgi <g_garenov@outlook.com>
2026-04-02 20:24:30 +0300

    One more project is added in the portfolio.

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,28 +1,37 @@
 export const projectData = [
   {
-      title: "Sunglasses Ecommerce Website",
-      description:
-        "A full-stack e-commerce application built with React and Node.js, featuring both shopping and admin interfaces.",
-      tech: ["React", "Tailwind CSS", "Vite", "Node JS", "Express", "Mongo DB"],
-      image: "/assets/images/project1.png",
-      liveDemo: "https://sunglasses-ecommerce-app.vercel.app/",
-      github: "https://github.com/GGarenov/ecommerce-app",
-    },
-    {
-      title: "Real Estate Marketplace",
-      description:
-        "A real estate marketplace built with the MERN stack. Users can browse, filter, and create property listings for rent or sale. Registered users can manage their profiles and listings, including profile pictures and property details.",
-      tech: [
-        "React",
-        "Redux",
-        "Tailwind CSS",
-        "Node JS",
-        "Express",
-        "Firebase",
-        "Render",
-      ],
-      image: "/assets/images/project2.png",
-      liveDemo: "https://real-estate-app-mern-qxrr.onrender.com/",
-      github: "https://github.com/GGarenov/real-estate-app",
-    }
-  ];
+    title: "Sunglasses Ecommerce Website",
+    description:
+      "A full-stack e-commerce application built with React and Node.js, featuring both shopping and admin interfaces.",
+    tech: ["React", "Tailwind CSS", "Vite", "Node JS", "Express", "Mongo DB"],
+    image: "/assets/images/project1.png",
+    liveDemo: "https://sunglasses-ecommerce-app.vercel.app/",
+    github: "https://github.com/GGarenov/ecommerce-app",
+  },
+  {
+    title: "Real Estate Marketplace",
+    description:
+      "A real estate marketplace built with the MERN stack. Users can browse, filter, and create property listings for rent or sale. Registered users can manage their profiles and listings, including profile pictures and property details.",
+    tech: [
+      "React",
+      "Redux",
+      "Tailwind CSS",
+      "Node JS",
+      "Express",
+      "Firebase",
+      "Render",
+    ],
+    image: "/assets/images/project2.png",
+    liveDemo: "https://real-estate-app-mern-qxrr.onrender.com/",
+    github: "https://github.com/GGarenov/real-estate-app",
+  },
+  {
+    title: "Simple HTML/CSS/JS Ecommerce Website",
+    description:
+      "Simple Figma to HTML/CSS/JS ecommerce one page website built with HTML, CSS, and JavaScript. ",
+    tech: ["HTML", "CSS", "JavaScript"],
+    image: "/assets/images/project3.png",
+    liveDemo: "https://ggarenov.github.io/vanilla-js-ecommerce/",
+    github: "https://github.com/GGarenov/vanilla-js-ecommerce",
+  },
+];
