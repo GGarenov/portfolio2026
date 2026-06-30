e596c90c093908b688065d809d53eb63c682ce4d
georgiyg1 <georgiy.g@first.bet>
2026-06-30 10:33:34 +0300

    Update project titles for clarity and remove outdated project entry

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -9,7 +9,7 @@ export const projectData = [
     github: "https://github.com/GGarenov/lastManStanding",
   },
   {
-    title: "Sunglasses Ecommerce Website",
+    title: "Sunglasses Ecommerce Website (React/Tailwind/NodeJS/MongoDB)",
     description:
       "A full-stack e-commerce application built with React and Node.js, featuring both shopping and admin interfaces.",
     tech: ["React", "Tailwind CSS", "Vite", "Node JS", "Express", "Mongo DB"],
@@ -18,7 +18,7 @@ export const projectData = [
     github: "https://github.com/GGarenov/ecommerce-app",
   },
   {
-    title: "Real Estate Marketplace",
+    title: "Real Estate Marketplace (React/Tailwind/Express/MongoDB/Firebase)",
     description:
       "A real estate marketplace built with the MERN stack. Users can browse, filter, and create property listings for rent or sale. Registered users can manage their profiles and listings, including profile pictures and property details.",
     tech: [
@@ -33,15 +33,6 @@ export const projectData = [
     image: "/assets/images/project2.png",
     liveDemo: "https://real-estate-app-mern-qxrr.onrender.com/",
     github: "https://github.com/GGarenov/real-estate-app",
-  },
-  {
-    title: "Simple HTML/CSS/JS Ecommerce Website",
-    description:
-      "Simple Figma to HTML/CSS/JS ecommerce one page website built with HTML, CSS, and JavaScript. ",
-    tech: ["HTML", "CSS", "JavaScript"],
-    image: "/assets/images/project3.png",
-    liveDemo: "https://ggarenov.github.io/vanilla-js-ecommerce/",
-    github: "https://github.com/GGarenov/vanilla-js-ecommerce",
   },
   {
     title: "Simple HTML/CSS/JS Ecommerce Website",
